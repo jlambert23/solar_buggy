963a7074bfce4ca06770533f0e6669a775300670
jlambert23 <jb.lambert23@gmail.com>
2018-09-02 20:25:44 +0000

    using other ultra sensors

--- solar_buggy/scripts/read_serial.py
+++ solar_buggy/scripts/read_serial.py
@@ -6,30 +6,24 @@ from std_msgs.msg import String
 
 serU = serial.Serial('/dev/ttyACM0', 9600)
 
-def publish_sensor_data(data):
-    port = data.data[0:4].strip('\r\n')
-    distance = data.data[5:].strip('\r\n')
-        
-    pub = rospy.Publisher('ultrasonic_' + port, String, queue_size=10)
-    pub.publish(distance)
-
 def ultra():
-    pub = rospy.Publisher('ultrasonic_1010', String, queue_size=10)
+
 
     rate = rospy.Rate(10) # 10hz
 
     while not rospy.is_shutdown():
         if serU.in_waiting > 0:
-        	dist = serU.readline()
+            data = serU.readline()
 
-        	#rospy.loginfo(dist)
-        	pub.publish(dist)
-        	rate.sleep()
+            port = data[0:4].strip('\r\n')
+            distance = data[5:].strip('\r\n')
 
-# Currently using pubsub for simulated serial data. Will replace as arduino uno is implemented.
-def read_serial():
-    rospy.Subscriber('ultrasonic', String, publish_sensor_data)
-    rospy.spin()
+            pub = rospy.Publisher('ultrasonic_' + port, String, queue_size=10)
+            pub.publish(distance)
+
+            #rospy.loginfo(data)
+            rate.sleep()
+
 
 if __name__ == '__main__':
     rospy.init_node('read_serial', anonymous=True)
--- solar_buggy/scripts/ultrasonic.py
+++ solar_buggy/scripts/ultrasonic.py
@@ -12,24 +12,22 @@ sensors = {
 }
 
 def handle_left(data):
-    distance = float(data.data)
-    rospy.loginfo('left: ' + data.data)
+    distance = int(data.data) * 10
+    #rospy.loginfo('left: ' + data.data)
     pub = rospy.Publisher('left_sensor', Int8, queue_size=10)
+    print(distance)
     
-    if distance >= config['proximity']['mid'] and not sensors['left']:
+    if distance >= 30 and not sensors['left']:
         sensors['left'] = True
         pub.publish(True)
-        # leftSensor = False
 
-    elif distance <= config['proximity']['low'] and sensors['left']:
+    elif distance < 30 and sensors['left']:
         sensors['left'] = False
         pub.publish(False)
-    #     leftSensor = False
-    #     lookingFromLeft = True
 
 def handle_front_left(data):
-    distance = float(data.data)
-    rospy.loginfo('front left: ' + data.data)
+    distance = int(data.data) * 10
+    #rospy.loginfo('front left: ' + data.data)
     pub = rospy.Publisher('front_left_sensor', Int8, queue_size=10)
 
     if distance > config['proximity']['high']:
@@ -52,8 +50,8 @@ def handle_front_left(data):
     #     leftFrontSensor = True
 
 def handle_front_right(data):
-    distance = float(data.data)
-    rospy.loginfo('front right: ' + data.data)
+    distance = int(data.data) * 10
+    #rospy.loginfo('front right: ' + data.data)
     pub = rospy.Publisher('front_right_sensor', Int8, queue_size=10)
 
     if distance > config['proximity']['high'] and not sensors['front_right']:
@@ -79,8 +77,8 @@ def handle_front_right(data):
     #     rightFrontSensor = True
 
 def handle_right(data):
-    distance = float(data.data)
-    rospy.loginfo('right: ' + data.data )
+    distance = int(data.data) * 10
+    #rospy.loginfo('right: ' + data.data )
     pub = rospy.Publisher('right_sensor', Int8, queue_size=10)
 
     if distance >= config['proximity']['mid'] and sensors['right']:
@@ -99,9 +97,9 @@ if __name__ == '__main__':
     try:
         rospy.init_node('ultrasonic', anonymous=True)
         rospy.Subscriber('ultrasonic_' + config['ports']['left'], String, handle_left)
-        rospy.Subscriber('ultrasonic_' + config['ports']['front_left'], String, handle_front_left)
-        rospy.Subscriber('ultrasonic_' + config['ports']['front_right'], String, handle_front_right)
-        rospy.Subscriber('ultrasonic_' + config['ports']['right'], String, handle_right)
+        #rospy.Subscriber('ultrasonic_' + config['ports']['front_left'], String, handle_front_left)
+        #rospy.Subscriber('ultrasonic_' + config['ports']['front_right'], String, handle_front_right)
+        #rospy.Subscriber('ultrasonic_' + config['ports']['right'], String, handle_right)
 
         rospy.spin()
 
