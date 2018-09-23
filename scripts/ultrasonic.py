77a09f5fe92cba35cbeef3059dd773e1531169a9
jlambert23 <jay@DESKTOP-KFH9RV2.localdomain>
2018-09-23 16:30:09 -0400

    fixed ultrasonic topic subscription

--- scripts/ultrasonic.py
+++ scripts/ultrasonic.py
@@ -9,16 +9,16 @@ pub = rospy.Publisher('ultra_cmd', String, queue_size=10)
 
 sensors_clear = {
     'left': True,
-    'front_left': True,
-    'front_right': True,
-    'right': True
+    'front': True,
+    'right': True,
+    'back': True
 }
     
 def handler(ultra):
     sensors_clear['left'] = ultra.left > 30.0
-    sensors_clear['front_left'] = ultra.frontleft > 30.0
-    sensors_clear['front_right'] = ultra.frontright > 30.0
-    sensors_clear['right'] = ultra.left > 30.0
+    sensors_clear['front'] = ultra.front > 30.0
+    sensors_clear['right'] = ultra.right > 30.0
+    sensors_clear['back'] = ultra.back > 30.0
     
     if not all(sensors_clear.values()):
         pub.publish('stop')
@@ -27,5 +27,5 @@ def handler(ultra):
 
 if __name__ == '__main__':
     rospy.init_node('ultrasonic', anonymous=True)
-    rospy.Subscriber('ultra_serial', Ultrasonic, handler)
+    rospy.Subscriber('ultrasonic', Ultrasonic, handler)
     rospy.spin()
