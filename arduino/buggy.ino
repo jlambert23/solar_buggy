e7e9fb3033146e7cf446359e3b639c6fe6f0b156
Andres Posadas <andres.posadas@knights.ucf.edu>
2018-10-05 17:25:05 -0400

    Cleaned up ino file and added IMU data to gps topic

--- arduino/buggy.ino
+++ arduino/buggy.ino
@@ -4,34 +4,16 @@
 #include <SPI.h>
 #include <SparkFunLSM9DS1.h>
 
-//////////////////////////
-// LSM9DS1 Library Init //
-//////////////////////////
-// Use the LSM9DS1 class to create an object. [imu] can be
-// named anything, we'll refer to that throught the sketch.
+// IMU prep variables
 LSM9DS1 imu;
-
-///////////////////////
-// Example I2C Setup //
-///////////////////////
-// SDO_XM and SDO_G are both pulled high, so our addresses are:
 #define LSM9DS1_M	0x1E // Would be 0x1C if SDO_M is LOW
 #define LSM9DS1_AG	0x6B // Would be 0x6A if SDO_AG is LOW
-
-NeoGPS::Location_t destination( 28.5853905, -81.2001379 );
-
 #define PRINT_CALCULATED
-//#define PRINT_RAW
-#define PRINT_SPEED 100 // 250 ms between prints
-static unsigned long lastPrint = 0; // Keep track of print time
-
-// Earth's magnetic field varies by location. Add or subtract 
-// a declination to get a more accurate heading. Calculate 
-// your's here:
-// http://www.ngdc.noaa.gov/geomag-web/#declination
+#define PRINT_SPEED 100 
 #define DECLINATION -8.58 // Declination (degrees) in Boulder, CO.
 
 // GPS prep variables.
+NeoGPS::Location_t destination( 28.5853905, -81.2001379 );
 static NMEAGPS  gps;
 static gps_fix  fix;
 
@@ -41,6 +23,7 @@ const int   echoPins[] = { 4, 6, 8, 10 };
 const int   trigPins[] = { 5, 7, 9, 11 };
 static long duration;
 static int  distance;
+static unsigned long lastPrint = 0; 
 
 // Json output objects
 DynamicJsonBuffer jsonBuffer;
@@ -69,12 +52,7 @@ void setup() {
   // and turns it on.
   if (!imu.begin())
   {
-    Serial.println("Failed to communicate with LSM9DS1.");
-    Serial.println("Double-check wiring.");
-    Serial.println("Default settings in this sketch will " \
-                  "work for an out of the box LSM9DS1 " \
-                  "Breakout, but may need to be modified " \
-                  "if the board jumpers are.");
+    Serial.println("Failed to communicate with IMU.");
     while (1)
       ;
   }
@@ -94,47 +72,31 @@ void loop() {
     ultrasonic();
   
       // Update the sensor values whenever new data is available
-  if ( imu.gyroAvailable() )
-  {
-    // To read from the gyroscope,  first call the
-    // readGyro() function. When it exits, it'll update the
-    // gx, gy, and gz variables with the most current data.
-    imu.readGyro();
-  }
-  if ( imu.accelAvailable() )
-  {
-    // To read from the accelerometer, first call the
-    // readAccel() function. When it exits, it'll update the
-    // ax, ay, and az variables with the most current data.
-    imu.readAccel();
-  }
-  if ( imu.magAvailable() )
-  {
-    // To read from the magnetometer, first call the
-    // readMag() function. When it exits, it'll update the
-    // mx, my, and mz variables with the most current data.
-    imu.readMag();
-  }
+    if ( imu.gyroAvailable() )
+    {
+      imu.readGyro();
+    }
+    if ( imu.accelAvailable() )
+    {
+      imu.readAccel();
+    }
+    if ( imu.magAvailable() )
+    {
+      imu.readMag();
+    }
   
-  if ((lastPrint + PRINT_SPEED) < millis())
-  {
-    //printGyro();  // Print "G: gx, gy, gz"
-    //printAccel(); // Print "A: ax, ay, az"
-    //printMag();   // Print "M: mx, my, mz"
-    // Print the heading and orientation for fun!
-    // Call print attitude. The LSM9DS1's mag x and y
-    // axes are opposite to the accelerometer, so my, mx are
-    // substituted for each other.
-    printAttitude(imu.ax, imu.ay, imu.az, 
-                 -imu.my, -imu.mx, imu.mz);
-    Serial.println();
-    
-    lastPrint = millis(); // Update lastPrint time
-  }
-  
-    root.printTo(Serial);
-    Serial.println();
-  }
+    if ((lastPrint + PRINT_SPEED) < millis())
+    {
+      printAttitude(imu.ax, imu.ay, imu.az, 
+                   -imu.my, -imu.mx, imu.mz);
+      // Serial.println();
+      
+      lastPrint = millis(); // Update lastPrint time
+    }
+
+      root.printTo(Serial);
+      Serial.println();
+    }
 }
 
 void ultrasonic()
@@ -174,10 +136,7 @@ void printAttitude(float ax, float ay, float az, float mx, float my, float mz)
   heading *= 180.0 / PI;
   pitch *= 180.0 / PI;
   roll  *= 180.0 / PI;
-  
-  //Serial.print("Pitch, Roll: ");
-  //Serial.print(pitch, 2);
-  //Serial.print(", ");
-  //Serial.println(roll, 2);
-  Serial.print("Heading: "); Serial.println(heading, 2);
-}
+
+  // Serial.print("Heading: "); Serial.println(heading, 2);
+
+  gpsJson["IMU"] = heading;
