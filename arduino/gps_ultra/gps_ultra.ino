317b4a1a013510fc958c5a0b310695eeadc9fb7a
Andres Posadas <Andres.Posadas@knights.ucf.edu>
2018-10-14 09:16:04 +1300

    added ultra arduino and gps config6969

--- arduino/gps_ultra/gps_ultra.ino
+++ arduino/gps_ultra/gps_ultra.ino
@@ -21,6 +21,11 @@ JsonObject& gpsJson = root.createNestedObject("gps");
 JsonObject& destJson = gpsJson.createNestedObject("waypoint");
 JsonObject& ultraJson = root.createNestedObject("ultrasonic");
 
+byte baudrateCmd[] = {160, 161, 00, 04, 05, 00, 05, 00, 00, 13, 10}; // 115200		A0 A1 00 04 05 00 05 00 00 0D 0A
+byte updateRate20HzCmd[] = {160, 161, 00, 03, 14, 20, 00, 26, 13, 10}; // 20 Hz			A0 A1 00 03 0E 14 00 1A 0D 0A
+byte updateRate10HzCmd[] = {160, 161, 00, 03, 14, 10, 00, 04, 13, 10}; // 10 Hz			A0 A1 00 03 0E 0A 00 04 0D 0A
+
+
 void setup() {
   for (int i = 0; i < ultrasonics; i++) {
     pinMode(echoPins[i], INPUT);
@@ -29,10 +34,18 @@ void setup() {
   
   Serial.begin(9600);
   Serial1.begin(38400); // GPS serial
+  
+  Serial1.write(baudrateCmd, sizeof(baudrateCmd));
+  Serial1.flush();
+  delay(10);
+  Serial1.begin(115200); // New baudrate
+  
+  Serial1.write(updateRate10HzCmd, sizeof(updateRate10HzCmd));
 }
 
 void loop() {  
-  while (gps.available( Serial1 )) {
+  
+  while (gps.available( Serial1 )) {  
     fix = gps.read();
 
     gpsJson["latitude"] = fix.latitude();
@@ -47,20 +60,24 @@ void loop() {
     root.printTo(Serial);
     Serial.println();
   }
+  
 }
 
 void ultrasonic()
 {
   // Read and print all ultrasonic sensors.
   for (int i = 0; i < ultrasonics; i++) {
+    
     digitalWrite(trigPins[i], LOW);
-    delay(1);
+    delayMicroseconds(2);
     digitalWrite(trigPins[i], HIGH);
-    delay(1);
+    delayMicroseconds(10);
     digitalWrite(trigPins[i], LOW);
     
     duration = pulseIn(echoPins[i], HIGH);
+    
     distance = duration*0.034/2;
+    distance = distance > 1000 ? 350 : distance;
 
     ultraJson[String(i)] = distance;
   }
