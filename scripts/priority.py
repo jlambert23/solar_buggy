ef668d549c032e7c7ea57db0b800c4465d0f9699
Justin <jb.lambert23@gmail.com>
2018-10-25 16:58:23 -0400

    implemented minheap in priority stack

--- scripts/priority.py
+++ scripts/priority.py
@@ -4,29 +4,52 @@ import rospy
 from solar_buggy.msg import Pose
 from solar_buggy.srv import Controller
 from std_msgs.msg import String
+import heapq
 
-control_pub = rospy.Publisher('cmd_vel', Pose, queue_size=10)
+priority = {'ultra_cmd': 1, 'cam_cmd': 2, 'gps_cmd': 3}
 
-def ultra_handler(pose):
-    #print(pose)
-    control_pub.publish(pose)
+''' We're using a MinHeap y'all! '''
+class PriorityStack:
 
-def gps_handler(command):
-    if (command.data == 'destination_reached'):
-        rospy.loginfo('Not today ISIS!!')
-        #controller('stop')
+    def __init__(self):
+        rospy.init_node('priority', anonymous=True)
 
-    #if (command.data != 'turn_around'):
-        #controller(command.data)
+        self.control_pub = rospy.Publisher('cmd_vel', Pose, queue_size=10)
+        self.heap = []
+    
+    def _send_command(self):
+        _, cmd = heapq.heappop(self.heap)
+        self.control_pub.publish(cmd)
 
-def camera_handler(command):
-    print(command.data)
-    controller(command.data)
+    def ultra_handler(self, pose):
+        
+        # Remove and replace command in heap
+        self.heap = [i for i in self.heap if x[0] == priority['ultra_cmd']]
+        heapq.heappush(self.heap, (priority['ultra_cmd'], pose))
+
+        self._send_command()
+
+    def cam_handler(self, pose):
+        
+        # Remove and replace command in heap
+        self.heap = [i for i in self.heap if x[0] == priority['cam_cmd']]
+        heapq.heappush(self.heap, (priority['cam_cmd'], pose))
+
+        self._send_command()
+
+    def gps_handler(self, pose):
+        
+        # Remove and replace command in heap
+        self.heap = [i for i in self.heap if x[0] == priority['gps_cmd']]
+        heapq.heappush(self.heap, (priority['gps_cmd'], pose))
+        
+        self._send_command()
 
 if __name__ == '__main__':
-    rospy.init_node('priority', anonymous=True)
-    
-    rospy.Subscriber('ultra_vel', Pose, ultra_handler)
-    #rospy.Subscriber('gps_vel', Pose, gps_handler)
+    ps = PriorityStack()
+
+    rospy.Subscriber('ultra_vel', Pose, ps.ultra_handler)
+    rospy.Subscriber('cam_vel', Pose, ps.cam_handler)
+    rospy.Subscriber('gps_vel', Pose, ps.gps_handler)
 
     rospy.spin()
